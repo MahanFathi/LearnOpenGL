05308c18ffbf0bb501c7e863db449138923b2f42
mahanfathi <mahan0fathi@gmail.com>
2020-01-23 21:29:27 +0330

    Spawn an army of Dr. Sakuee

--- main.cpp
+++ main.cpp
@@ -73,6 +73,18 @@ int main()
         1, 7, 2,
         2, 6, 7,
     };
+    glm::vec3 cubePositions[] = {   // spawn cubes at these positions
+      glm::vec3( 0.0f,  0.0f,  0.1f),
+      glm::vec3( 2.0f,  5.0f, -15.0f), 
+      glm::vec3(-1.5f, -2.2f, -2.5f),  
+      glm::vec3(-3.8f, -2.0f, -12.3f),  
+      glm::vec3( 2.4f, -0.4f, -3.5f),  
+      glm::vec3(-1.7f,  3.0f, -7.5f),  
+      glm::vec3( 1.3f, -2.0f, -2.5f),  
+      glm::vec3( 1.5f,  2.0f, -2.5f), 
+      glm::vec3( 1.5f,  0.2f, -1.5f), 
+      glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     // generate and bind vertex array object
     GLuint VAO;
@@ -142,13 +154,12 @@ int main()
     // glBindVertexArray(0);
 
     //  make transformations
-    glm::mat4 transform = glm::mat4(1.0f);
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 10.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
     shader.setUniform("model", model);
     shader.setUniform("view", view);
     shader.setUniform("projection", projection);
@@ -175,11 +186,23 @@ int main()
         greenValue = sin(time) / 2.0f + 0.5f;
         shader.setUniform("greenValue", greenValue);
 
-        // rotate
-        transform = glm::rotate(transform, time/1000, glm::vec3(0.0f, 0.0f, 1.0f));
-        shader.setUniform("transform", transform);
 
-        glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, (void*)0);
+        for (auto position : cubePositions) {
+
+            // rotate
+            glm::mat4 transform = glm::mat4(1.0f);
+            transform = glm::rotate(transform, time, glm::normalize(position));
+            shader.setUniform("transform", transform);
+
+            // translate
+            glm::mat4 modelTranslation = glm::mat4(1.0f);
+            modelTranslation = glm::translate(modelTranslation, position);
+            shader.setUniform("model", modelTranslation * model);
+
+            glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, (void*)0);
+
+        }
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
