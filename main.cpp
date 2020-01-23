a645e4bce906a11eb10b13e643e33628864ca84b
mahanfathi <mahan0fathi@gmail.com>
2020-01-23 22:12:49 +0330

    Add rotating cam

--- main.cpp
+++ main.cpp
@@ -158,10 +158,7 @@ int main()
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
-    shader.setUniform("model", model);
-    shader.setUniform("view", view);
     shader.setUniform("projection", projection);
 
 
@@ -186,6 +183,12 @@ int main()
         greenValue = sin(time) / 2.0f + 0.5f;
         shader.setUniform("greenValue", greenValue);
 
+        // rotate cam by chanding view matrix
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        shader.setUniform("view", view);
 
         for (auto position : cubePositions) {
 
