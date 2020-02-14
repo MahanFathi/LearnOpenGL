31b8d7a4caad2b23b5187e99fe10f422fea47e7f
MahanFathi <mahan0fathi@gmail.com>
2020-02-15 01:01:49 +0330

    Add texture to boxes

--- etc/object_shaders/fragment_shader.glsl
+++ etc/object_shaders/fragment_shader.glsl
@@ -5,6 +5,7 @@ struct Material {
     float diffuseStrength;
     float specularStrength;
     float shininess;
+    sampler2D textureSampler;
 };
 
 in vec3 fragNormal;
@@ -33,5 +34,8 @@ void main()
     float specular = material.specularStrength *
         pow(max(dot(viewDirection, reflectDirection), 0.0), material.shininess);
 
-    FragColor = vec4((specular + diffusion + material.ambientStrength) * lightColor * cubeColor, 1.0);
+    FragColor = texture(material.textureSampler, fragTexCoords) + vec4((specular + diffusion + material.ambientStrength) * lightColor * cubeColor, 1.0);
+
+    // FragColor = mix(texture(textureSampler0, txtr), texture(textureSampler1, txtr), 0.2) +
+        // vec4(color.r, color.g + greenValue, color.b, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -101,12 +101,12 @@ int main()
         -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
     };
     glm::vec3 cubePositions[] = {   // spawn cubes at these positions
       glm::vec3( 0.0f,  0.0f,  0.1f),
@@ -141,6 +141,25 @@ int main()
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
     glEnableVertexAttribArray(2);
+
+    // generate and bind texture
+    GLuint texture;
+    glGenTextures(1, &texture);
+
+    // texture pattern, filtering, and image #0
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int textureWidth, textureHeight, textureChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* textureImage = stbi_load("./resources/textures/2.jpg", &textureWidth, &textureHeight, &textureChannels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, textureImage);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(textureImage);
+    objectShader.setUniform("material.textureSampler", 0);
 
     // glBindBuffer(GL_ARRAY_BUFFER, 0);
     // glBindVertexArray(0);
